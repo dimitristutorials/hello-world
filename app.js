f3003cf1a0850f0f6d7887683225ff0b5d18e45f
dimitristutorials <66831443+dimitristutorials@users.noreply.github.com>
2026-02-28 12:01:28 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+const message = process.env.MESSAGE
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Hello World Update!');
+  res.send(message);
 });
 
 // Start server
